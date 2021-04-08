9bfb7abbc7de9de4b3f472485879effa9da27605
diogoloiola <diogoloiola666@gmail.com>
2021-04-08 15:49:28 -0300

    main ja configurado e recebendo dados via terminal

--- tema 1/main.py
+++ tema 1/main.py
@@ -1,10 +1,24 @@
+from os import error
 import sys
-
+import Graph
 if __name__ == "__main__":
-    f = open('test/ex1.txt','r')
-    u, v = f.readline().split(' ')
-    u = int(u)
-    v = int(v)
-    for j in range(0, u):
-        edge = f.readline().replace("\n","").split(" ")
-        print(edge)
+    try:
+        if len(sys.argv) == 1:
+           raise error('arquivo de entrada nao fornecido')
+        else:
+            f = open(sys.argv[1],'r')
+            u, v = f.readline().split(' ')
+            u = int(u)
+            v = int(v)
+            matriz = []
+            for j in range(0, u):
+                edge = f.readline().replace("\n","").split(" ")
+                matriz.append(list(map(int, edge)))
+            a = Graph.Grafo(u)
+            a.grafo = matriz
+            if a.eBipartido():
+                print('possui ciclos pares')
+            else:
+                print('nao possui ciclos pares')
+    except:
+        raise error('erro ao abrir o arquivo')
